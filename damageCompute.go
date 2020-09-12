a2603017693611245669b766557462d0f8773e4c
Will Lehman <william.p.lehman@usace.army.mil>
2020-09-11 21:10:03 -0700

    adding better handling of fringe cases, testing fringes

--- damageCompute.go
+++ damageCompute.go
@@ -34,10 +34,41 @@ func main(){
 
 	//simplified compute
 	ret := computeStructureDamageAtStructure(s,d)
-	fmt.Println(ret)
+	fmt.Println("for a depth of", d.depth, "the damage is",ret)
+
+
+
+	d.depth = 0.0 // test lower case
+	ret = computeStructureDamageAtStructure(s,d)
+	fmt.Println("for a depth of", d.depth, "the damage is",ret)
+
+	d.depth = 1.0 // test lowest valid case
+	ret = computeStructureDamageAtStructure(s,d)
+	fmt.Println("for a depth of", d.depth, "the damage is",ret)
+
+	d.depth = 2.5 //test interpolation case (not passing currently)
+	ret = computeStructureDamageAtStructure(s,d)
+	fmt.Println("for a depth of", d.depth, "the damage is",ret)
+
+	d.depth = 4.0 // test highest valid case
+	ret = computeStructureDamageAtStructure(s,d)
+	fmt.Println("for a depth of", d.depth, "the damage is",ret)
+	
+	d.depth = 5.0 //test upper case
+	ret = computeStructureDamageAtStructure(s,d)
+	fmt.Println("for a depth of", d.depth, "the damage is",ret)
+
+
 }
 func computeStructureDamageAtStructure(s structure,d depthEvent) float64{
+	if d.depth < s.occType.damfun.xvals[0]{
+		return 0.0 //depth is less than lowest damage value
+	}
+	size := len(s.occType.damfun.xvals)
+	if d.depth >= s.occType.damfun.xvals[size-1]{
+		return (s.occType.damfun.yvals[size-1]/100.0)*s.structVal //depth yeilds largest damage value
+	}
 	lower := sort.SearchFloat64s(s.occType.damfun.xvals,d.depth)
-	fmt.Println(lower)
+	//interpolate
 	return (s.occType.damfun.yvals[lower]/100.0)*s.structVal
 }
