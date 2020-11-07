bbe5395c5ba9f8fdfbc7909f038f9d13155d0b74
Will Lehman <william.p.lehman@usace.army.mil>
2020-11-06 19:32:36 -0800

    got bitflags working on hazards, not sure this is a useful exercise.

--- hazards/flood_test.go
+++ hazards/flood_test.go
@@ -6,17 +6,30 @@ import (
 )
 
 func TestParameters(t *testing.T) {
-	d := DepthEvent{Depth: 2.5, parameter: Depth}
+	d := DepthEvent{Depth: 2.5, parameter: Default}
 	//p := Depth
+	d.parameter = SetHasDepth(d.parameter)
+	d.parameter = SetHasVelocity(d.parameter)
 	//d.parameter = p
+	fmt.Println(d.parameter.String())
 	if d.Has(Depth) {
-		fmt.Println("True")
+		fmt.Println("Has Depth")
 	} else {
-		fmt.Println("False!!")
+		fmt.Println("Hasnt depth")
 	}
 	if d.Has(ArrivalTime) {
-		fmt.Println("False!!!")
+		fmt.Println("Has Arrival Time")
 	} else {
-		fmt.Println("True!!")
+		fmt.Println("Hasnt arrival time")
+	}
+	if d.Has(ArrivalTime2ft) {
+		fmt.Println("Has Arrival Time 2ft")
+	} else {
+		fmt.Println("Hasnt arrival time 2ft")
+	}
+	if d.Has(Velocity) {
+		fmt.Println("Has Velocity")
+	} else {
+		fmt.Println("Hasnt Velocity")
 	}
 }
--- hazards/interfaces.go
+++ hazards/interfaces.go
@@ -12,28 +12,76 @@ type Hazards interface {
 }
 
 //https://github.com/yourbasic/bit a possible place to expand the set of hazards
-type Parameter uint8
+type Parameter byte
 
 const (
-	Depth          Parameter = iota //0
-	Velocity       Parameter = iota // 1
-	ArrivalTime    Parameter = iota //2
-	ArrivalTime2ft Parameter = iota // 3
-	Duration       Parameter = iota
+	Default        Parameter = 0  //0
+	Depth          Parameter = 1  //1
+	Velocity       Parameter = 2  //2
+	ArrivalTime    Parameter = 4  //3
+	ArrivalTime2ft Parameter = 8  //4
+	Duration       Parameter = 16 //5
+	//next parameter
+	//next parameter
+	//next parameter
+	//fin
+
 )
 
-func (h Parameter) SetHasDepth() {
-	h = h | Depth
+func SetHasDepth(h Parameter) Parameter {
+	return h | Depth
 }
-func (h Parameter) SetHasVelocity() {
-	h = h | Velocity
+func SetHasVelocity(h Parameter) Parameter {
+	return h | Velocity
 }
-func (h Parameter) SetHasArrivalTime() {
-	h = h | ArrivalTime
+func SetHasArrivalTime(h Parameter) Parameter {
+	return h | ArrivalTime
 }
-func (h Parameter) SetHasArrivalTime2ft() {
-	h = h | ArrivalTime2ft
+func SetHasArrivalTime2ft(h Parameter) Parameter {
+	return h | ArrivalTime2ft
 }
-func (h Parameter) SetHasDuration() {
-	h = h | Duration
+func SetHasDuration(h Parameter) Parameter {
+	return h | Duration
+}
+
+func (p Parameter) String() string {
+	s := ""
+	count := 0
+	if p&Depth != 0 {
+		s += "Depth"
+		count += 1
+	}
+	if p&ArrivalTime != 0 {
+		if count > 0 {
+			s += ", "
+		}
+		s += "Arrival Time"
+
+		count += 1
+	}
+	if p&ArrivalTime2ft != 0 {
+		if count > 0 {
+			s += ", "
+		}
+		s += "Arrival Time 2ft"
+
+		count += 1
+	}
+	if p&Velocity != 0 {
+		if count > 0 {
+			s += ", "
+		}
+		s += "Velocity"
+
+		count += 1
+	}
+	if p&Duration != 0 {
+		if count > 0 {
+			s += ", "
+		}
+		s += "Duration"
+
+		count += 1
+	}
+	return s
 }
