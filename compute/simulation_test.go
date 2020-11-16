efabe79480a3d9f718ce5ba6795d500b82ec59c0
rnugent3 <richard.nugent.3@gmail.com>
2020-11-16 08:58:49 -0800

    add EAD test

--- compute/simulation_test.go
+++ compute/simulation_test.go
@@ -25,6 +25,15 @@ func TestComputeEAD(t *testing.T) {
 	f := []float64{.75, .5, .25, 0}
 	val := computeEAD(d, f)
 	if val != 2.0 {
-		t.Errorf("computeEAD() yeilded %f; expected %f", val, 2.0)
+		t.Errorf("computeEAD() yielded %f; expected %f", val, 2.0)
+	}
+}
+
+func TestComputeEAD2(t *testing.T) {
+	d := []float64{1, 10, 30, 45, 59, 78, 89, 102, 140, 180, 240, 330, 350, 370}
+	f := []float64{.99, .95, .9, .8, .7, .6, .5, .4, .3, .2, .1, .01, .002, .001}
+	val := computeEAD(d, f)
+	if val != 113.125 {
+		t.Errorf("computeEAD() yielded %f; expected %f", val, 113.125)
 	}
 }
--- go.mod
+++ go.mod
@@ -5,5 +5,5 @@ go 1.15
 require (
 	github.com/HenryGeorgist/go-statistics v0.0.0-20200926020757-01f9d4902f65
 	github.com/aws/aws-lambda-go v1.19.1
-	github.com/dewberry/gdal v0.0.0-20201021153548-9bbc306059d3
+	github.com/dewberry/gdal v0.0.0-20201021153548-9bbc306059d3 // indirect
 )
