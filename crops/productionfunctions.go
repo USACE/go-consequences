f3effab2afa4f69c421b4e722cbf0b03eca2e723
Will Lehman <william.p.lehman@usace.army.mil>
2020-12-28 18:31:05 -0800

    testing production functions and updating production functions to pass test

--- crops/productionfunctions.go
+++ crops/productionfunctions.go
@@ -19,51 +19,64 @@ func NewProductionFunction(mc []float64, cs CropSchedule, hc float64, latePlanti
 	pf.productionCostLessHarvest = pclh
 	return pf
 }
-func isLeapYear(year int) bool{  
-	leapFlag := false  
-	if year%4 == 0 {  
-	 if year%100 == 0 {  
-	  if year%400 == 0 {  
-	   leapFlag = true  
-	  } else {  
-	   leapFlag = false  
-	  }  
-	 } else {  
-	  leapFlag = true  
-	 }  
-	} else {  
-	 leapFlag = false  
-	}  
-	return leapFlag  
+func isLeapYear(year int) bool {
+	leapFlag := false
+	if year%4 == 0 {
+		if year%100 == 0 {
+			if year%400 == 0 {
+				leapFlag = true
+			} else {
+				leapFlag = false
+			}
+		} else {
+			leapFlag = true
+		}
+	} else {
+		leapFlag = false
+	}
+	return leapFlag
+}
+func (p productionFunction) GetCumulativeMonthlyProductionCosts() []float64 {
+	return p.cumulativeMonthlyProductionCosts
 }
 func cumulateMonthlyCosts(mc []float64, cs CropSchedule) ([]float64, float64) {
-	//check for winter crops.
+	//this process assumes days to maturity is less than 1 year.
 	totalCosts := 0.0
 	cmc := make([]float64, 12)
-	if cs.StartPlantingDate.YearDay()+cs.DaysToMaturity > 365 {
-		//winter crop.
-		daysInYear := 365
-		if isLeapYear(cs.StartPlantingDate.Year()){
-			daysInYear +=1
-		}
-
-	} else {
-		//contained between 0 and 365 days
-		startMonth := cs.StartPlantingDate.Month() //iota "enum"
-		startMonthIndex := int(startMonth)
-		counter := 0
-		daysToMaturity := cs.DaysToMaturity
-		for ok := true; ok; ok = daysToMaturity > 0 {
-			//compute days in the current month https://yourbasic.org/golang/last-day-month-date/
-			t := time.Date(cs.StartPlantingDate.Year(), time.Month(startMonthIndex+counter+1), 0, 0, 0, 0, 0, time.UTC)
-			daysInMonth := t.Day() //subtract the days in the current month from days to maturity.
-			daysToMaturity -= daysInMonth
-			totalCosts += mc[startMonthIndex+counter]
-			cmc[startMonthIndex+counter] = totalCosts
-			counter++
-		}
-
+	daysInYear := 365
+	if isLeapYear(cs.StartPlantingDate.Year()) {
+		daysInYear += 1
 	}
-
+	if cs.DaysToMaturity > daysInYear {
+		panic("abort! abort! we hit an artery!")
+	}
+	startMonth := cs.StartPlantingDate.Month() //iota "enum"
+	startMonthIndex := int(startMonth) - 1
+	counter := 0
+	daysToMaturity := cs.DaysToMaturity
+	updated := false
+	year := cs.StartPlantingDate.Year()
+	for ok := true; ok; ok = daysToMaturity > 0 {
+		//compute days in the current month https://yourbasic.org/golang/last-day-month-date/
+		t := time.Date(year, time.Month(startMonthIndex+counter+1), 0, 0, 0, 0, 0, time.UTC)
+		daysInMonth := t.Day() //subtract the days in the current month from days to maturity.
+		if counter == 0 {
+			if !updated {
+				daysInMonth -= cs.StartPlantingDate.Day() //remove the start of the month where crops weren't planted
+			}
+		}
+		daysToMaturity -= daysInMonth
+		if startMonthIndex+counter > 11 {
+			if !updated {
+				startMonthIndex = 0
+				counter = 0
+				year++
+				updated = true
+			}
+		}
+		totalCosts += mc[startMonthIndex+counter]
+		cmc[startMonthIndex+counter] = totalCosts
+		counter++
+	}
 	return cmc, totalCosts
 }
---
+++ crops/productionfunctions_test.go
@@ -0,0 +1,55 @@
+package crops
+
+import (
+	"testing"
+	"time"
+)
+
+func TestCreateProductionFunction(t *testing.T) {
+	st := time.Date(1984, time.Month(1), 22, 0, 0, 0, 0, time.UTC)
+	et := time.Date(1984, time.Month(1), 28, 0, 0, 0, 0, time.UTC)
+	cs := CropSchedule{StartPlantingDate: st, LastPlantingDate: et, DaysToMaturity: 330}
+
+	mc := []float64{1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0}
+	expected := []float64{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0}
+	pf := NewProductionFunction(mc, cs, 1.0, .1)
+
+	for idx := range mc {
+		got := pf.GetCumulativeMonthlyProductionCosts()[idx]
+		if got != expected[idx] {
+			t.Errorf("productionFunction.GetCumulativeMonthlyProductionCosts()[%v] = %f; expected %f", idx, got, expected[idx])
+		}
+	}
+}
+func TestCreateProductionFunctionWrapYear(t *testing.T) {
+	st := time.Date(1984, time.Month(2), 22, 0, 0, 0, 0, time.UTC)
+	et := time.Date(1984, time.Month(2), 28, 0, 0, 0, 0, time.UTC)
+	cs := CropSchedule{StartPlantingDate: st, LastPlantingDate: et, DaysToMaturity: 330}
+
+	mc := []float64{1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0}
+	expected := []float64{12.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0}
+	pf := NewProductionFunction(mc, cs, 1.0, .1)
+
+	for idx := range mc {
+		got := pf.GetCumulativeMonthlyProductionCosts()[idx]
+		if got != expected[idx] {
+			t.Errorf("productionFunction.GetCumulativeMonthlyProductionCosts()[%v] = %f; expected %f", idx, got, expected[idx])
+		}
+	}
+}
+func TestCreateProductionFunction_ShorterCropSchedule(t *testing.T) {
+	st := time.Date(1984, time.Month(2), 22, 0, 0, 0, 0, time.UTC)
+	et := time.Date(1984, time.Month(2), 28, 0, 0, 0, 0, time.UTC)
+	cs := CropSchedule{StartPlantingDate: st, LastPlantingDate: et, DaysToMaturity: 299}
+
+	mc := []float64{1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0}
+	expected := []float64{0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0}
+	pf := NewProductionFunction(mc, cs, 1.0, .1)
+
+	for idx := range mc {
+		got := pf.GetCumulativeMonthlyProductionCosts()[idx]
+		if got != expected[idx] {
+			t.Errorf("productionFunction.GetCumulativeMonthlyProductionCosts()[%v] = %f; expected %f", idx, got, expected[idx])
+		}
+	}
+}
