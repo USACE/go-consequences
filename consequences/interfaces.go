4135198af661776a0c9bba6ac4c35b5f9c993402
Will Lehman <william.p.lehman@usace.army.mil>
2021-01-01 13:02:06 -0800

    adding comments for the consequences package

--- consequences/interfaces.go
+++ consequences/interfaces.go
@@ -7,17 +7,24 @@ import (
 	"github.com/HenryGeorgist/go-statistics/statistics"
 )
 
+//ConsequenceReceptor is an interface for all things that can have consequences from a hazard event
 type ConsequenceReceptor interface {
 	ComputeConsequences(event interface{}) ConsequenceDamageResult
 }
+
+//ConsequenceDamageResult is essentially a table of header string values and rows of objects which could be a single value or an array of values
 type ConsequenceDamageResult struct {
 	Headers []string
 	Results []interface{}
 }
+
+//Locatable is an interface that defines that a thing can have an x and y location
 type Locatable interface {
 	GetX() float64
 	GetY() float64
 }
+
+//ParameterValue is a way to allow parameters to be either a scalar or a distribution.
 type ParameterValue struct {
 	Value interface{}
 }
@@ -39,9 +46,13 @@ func (p ParameterValue) SampleValue(input interface{}) float64 {
 
 	return 0
 }
+
+//MarshalJSON converts ConsequenceDamage Result into an empty byte array (it should convert it into a json object... but it doesnt right now)
 func (c ConsequenceDamageResult) MarshalJSON() ([]byte, error) {
 	return make([]byte, 0), nil
 }
+
+//String converts a ConsequenceDamageResult to a string
 func (c ConsequenceDamageResult) String() string {
 	if len(c.Headers) != len(c.Results) {
 		return "mismatched lengths"
