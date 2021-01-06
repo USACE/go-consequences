d43db9a09ba66cb873c98b072facc530388f5211
Will Lehman <william.p.lehman@usace.army.mil>
2021-01-05 19:47:37 -0800

    Refactoring to unexport crop properties and to leverage the buildcrop method

--- crops/crop.go
+++ crops/crop.go
@@ -15,14 +15,16 @@ import (
 
 //Crop describes a crop that can be used to compute agricultural consequences
 type Crop struct {
-	ID                 int
-	Name               string
-	Yeild              float64
-	PricePerUnit       float64
-	ValuePerOutputUnit float64
-	ProductionFunction productionFunction
-	LossFunction       DamageFunction
-	CropSchedule       CropSchedule
+	id                 byte
+	name               string
+	x                  float64
+	y                  float64
+	yeild              float64
+	pricePerUnit       float64
+	valuePerOutputUnit float64
+	productionFunction productionFunction
+	lossFunction       DamageFunction
+	cropSchedule       CropSchedule
 }
 
 //xmlCrop is used for reading xml files not for any other real purpose
