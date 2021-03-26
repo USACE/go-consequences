51ca45986fb6b4ba76eda2eaf00c894377b5ca16
Will Lehman <william.p.lehman@usace.army.mil>
2021-03-26 18:57:04 -0400

    adding a test for state counts

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/USACE/go-consequences
 go 1.15
 
 require (
-	github.com/HenryGeorgist/go-statistics v0.0.0-20210125164034-3542879c2b55
-	github.com/aws/aws-lambda-go v1.22.0
+	github.com/HenryGeorgist/go-statistics v0.0.0-20210320023908-0c2f15b2445f
 	github.com/dewberry/gdal v0.3.1
 )
--- structureprovider/nsi-api_test.go
+++ structureprovider/nsi-api_test.go
@@ -32,10 +32,16 @@ func TestNsiByFipsStream_MultiState(t *testing.T) {
 	for ss := range f {
 		go func(sfips string) {
 			defer wg.Done()
+			index := 0
 			n.ByFips(sfips, func(s consequences.Receptor) {
 				index++
 			})
-			fmt.Println("Completed " + sfips)
+			fmt.Println(fmt.Sprintf("Completed %s with %v structures", sfips, index))
+			if countByState(sfips) == index {
+				fmt.Println(fmt.Sprintf("For state %s the count matched", sfips))
+			} else {
+				fmt.Println(fmt.Sprintf("For state %s the count did NOT match!", sfips))
+			}
 		}(ss)
 	}
 	wg.Wait()
@@ -74,4 +80,61 @@ func Test_StructureProvider_NSI_BBOX(t *testing.T) {
 		s, _ := c.(structures.StructureStochastic)
 		fmt.Println(s.Name)
 	})
+}
+
+func countByState(ss string) int {
+	m := make(map[string]int)
+	m["01"] = 2110979
+	m["02"] = 293688
+	m["04"] = 2668111
+	m["05"] = 1263237
+	m["06"] = 11111929
+	m["08"] = 2045575
+	m["09"] = 1234236
+	m["10"] = 381352
+	m["11"] = 153062
+	m["12"] = 7694670
+	m["13"] = 3810113
+	m["15"] = 344996
+	m["16"] = 666303
+	m["17"] = 4066775
+	m["18"] = 2613847
+	m["19"] = 1304639
+	m["20"] = 1205171
+	m["21"] = 1607171
+	m["22"] = 1872003
+	m["23"] = 607351
+	m["24"] = 2155044
+	m["25"] = 2042137
+	m["26"] = 4329378
+	m["27"] = 2167795
+	m["28"] = 1371025
+	m["29"] = 2544779
+	m["30"] = 501699
+	m["31"] = 785530
+	m["32"] = 1017555
+	m["33"] = 542509
+	m["34"] = 2982407
+	m["35"] = 800302
+	m["36"] = 4737239
+	m["37"] = 4266543
+	m["38"] = 312468
+	m["39"] = 4510863
+	m["40"] = 1617867
+	m["41"] = 1539773
+	m["42"] = 4870271
+	m["44"] = 375415
+	m["45"] = 1932100
+	m["46"] = 371769
+	m["47"] = 2578884
+	m["48"] = 9083959
+	m["49"] = 938757
+	m["50"] = 229969
+	m["51"] = 3466599
+	m["53"] = 2647194
+	m["54"] = 773467
+	m["55"] = 1989245
+	m["56"] = 275446
+
+	return m[ss]
 }
