970312a5ea5c893cb1b3739b5d91fda4ebdb7dba
Will Lehman <william.p.lehman@usace.army.mil>
2021-03-29 18:17:06 -0400

    adding a summary results writer for structure computes

--- consequences/summary-results-writer.go
+++ consequences/summary-results-writer.go
@@ -1,10 +1,12 @@
 package consequences
 
 import (
+	"fmt"
 	"io"
 	"os"
 
 	"github.com/HenryGeorgist/go-statistics/data"
+	"github.com/leekchan/accounting"
 )
 
 type summaryResultsWriter struct {
@@ -15,7 +17,7 @@ type summaryResultsWriter struct {
 	m          map[string]*data.InlineHistogram
 }
 
-func InitSummaryResultsWriterFromFile(filepath string) summaryResultsWriter {
+func InitSummaryResultsWriterFromFile(filepath string) *summaryResultsWriter {
 	w, err := os.OpenFile(filepath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0600)
 	if err != nil {
 		panic(err)
@@ -23,12 +25,12 @@ func InitSummaryResultsWriterFromFile(filepath string) summaryResultsWriter {
 	//make the maps
 	t := make(map[string]float64, 1)
 	m := make(map[string]*data.InlineHistogram, 1)
-	return summaryResultsWriter{filepath: filepath, w: w, totals: t, m: m}
+	return &summaryResultsWriter{filepath: filepath, w: w, totals: t, m: m}
 }
-func InitSummaryResultsWriter(w io.Writer) summaryResultsWriter {
-	return summaryResultsWriter{filepath: "not applicapble", w: w}
+func InitSummaryResultsWriter(w io.Writer) *summaryResultsWriter {
+	return &summaryResultsWriter{filepath: "not applicapble", w: w}
 }
-func (srw summaryResultsWriter) Write(r Result) {
+func (srw *summaryResultsWriter) Write(r Result) {
 	//hardcoding for structures to experiment and think it through.
 	var cat = "damage category"
 	var structDam = "structure damage"
@@ -66,7 +68,17 @@ func (srw summaryResultsWriter) Write(r Result) {
 		srw.totals[damcat] = totDam
 	}
 }
-func (srw summaryResultsWriter) Close() {
+func (srw *summaryResultsWriter) Close() {
+	ac := accounting.Accounting{Symbol: "$", Precision: 2}
+	fmt.Fprintf(srw.w, fmt.Sprintf("Grand Total is %v\n", ac.FormatMoney(srw.grandTotal)))
+	h := srw.totals
+	for i, v := range h {
+		fmt.Fprintf(srw.w, fmt.Sprintf("Damages for %v were %v\n", i, ac.FormatMoney(v)))
+	}
+	j := srw.m
+	for i, v := range j {
+		fmt.Fprintf(srw.w, fmt.Sprintf("Histogram for %v:\n%v", i, v.StringSparse()))
+	}
 	w2, ok := srw.w.(io.WriteCloser)
 	if ok {
 		w2.Close()
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/USACE/go-consequences
 go 1.15
 
 require (
-	github.com/HenryGeorgist/go-statistics v0.0.0-20210320023908-0c2f15b2445f
+	github.com/HenryGeorgist/go-statistics v0.0.0-20210329221111-af13727c6803
 	github.com/dewberry/gdal v0.3.1
+	github.com/dustin/go-humanize v1.0.0
+	github.com/leekchan/accounting v1.0.0
 )
