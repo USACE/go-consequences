22cc887890d3021045d18c5c5221e98fe95157ce
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-22 15:38:45 +0800

    Delete crops directory

--- crops/interfaces.go
+++
@@ -1,7 +0,0 @@
-package crops
-
-//CropType describes the relationship of the byte value in a NASSCDL to the Crop name (crop category in their terminology)
-type CropType interface {
-	GetCropID() byte
-	GetCropCategory() string
-}
