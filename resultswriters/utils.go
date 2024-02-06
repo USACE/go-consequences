6aff1333b84967ea6963d59c3dcb53d0419bc881
Will Lehman <william.p.lehman@usace.army.mil>
2024-02-06 19:51:23 +0000

    updating to allow for any spatial driver from ogr to write results

--- resultswriters/utils.go
+++ resultswriters/utils.go
@@ -23,10 +23,12 @@ const (
 	GPKG    ResultsWriterType = "GPKG"    //2
 	SHP     ResultsWriterType = "SHP"
 	PARQUET ResultsWriterType = "Parquet"
+	OGR     ResultsWriterType = "OGR"
 )
 
 type ResultsWriterInfo struct {
 	Type     ResultsWriterType `json:"results_writer_type"`
+	Driver   string            `json:"results_writer_driver,omitempty"`
 	FilePath string            `json:"output_file_path"`
 }
 
@@ -40,6 +42,8 @@ func (info ResultsWriterInfo) CreateResultsWriter() (consequences.ResultsWriter,
 		return InitShpResultsWriter(info.FilePath, "results")
 	case PARQUET:
 		return InitGeoparquetResultsWriter(info.FilePath, "results")
+	case OGR:
+		return InitSpatialResultsWriter(info.FilePath, "results", info.Driver)
 	default:
 		return nil, errors.New("could not create a result writer of that type")
 	}
