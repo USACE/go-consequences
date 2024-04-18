abd60e7bc2b337440a34a015693be13f76f433b1
Will Lehman <william.p.lehman@usace.army.mil>
2024-04-18 14:13:43 +0000

    adding in examples

--- lifeloss/stability_test.go
+++ lifeloss/stability_test.go
@@ -1,72 +1,90 @@
-package lifeloss
+package lifeloss_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/USACE/go-consequences/hazards"
+	"github.com/USACE/go-consequences/lifeloss"
 )
 
-func Test_RescDamUnanchoredWoodStability(t *testing.T) {
+func ExampleStabilityEvaluation() {
 
-	sc := RescDamWoodUnanchored
+	sc := lifeloss.RescDamWoodUnanchored
 
 	h := hazards.DepthandDVEvent{}
 	h.SetDV(30)
 	h.SetDepth(100)
 	result := sc.Evaluate(h)
-	if result != Stable {
+	fmt.Println(result)
+
+	h.SetDV(35)
+	h.SetDepth(100)
+	result = sc.Evaluate(h)
+	fmt.Println(result)
+}
+
+func Test_RescDamUnanchoredWoodStability(t *testing.T) {
+
+	sc := lifeloss.RescDamWoodUnanchored
+
+	h := hazards.DepthandDVEvent{}
+	h.SetDV(30)
+	h.SetDepth(100)
+	result := sc.Evaluate(h)
+	if result != lifeloss.Stable {
 		t.Fail()
 	}
 
 	h.SetDV(35)
 	h.SetDepth(100)
 	result = sc.Evaluate(h)
-	if result != Collapsed {
+	if result != lifeloss.Collapsed {
 		t.Fail()
 	}
 }
 func Test_RescDamAnchoredWoodStability(t *testing.T) {
 
-	sc := RescDamWoodAnchored
+	sc := lifeloss.RescDamWoodAnchored
 
 	h := hazards.DepthandDVEvent{}
 	h.SetDV(35)
 	h.SetDepth(100)
 	result := sc.Evaluate(h)
-	if result != Stable {
+	if result != lifeloss.Stable {
 		t.Fail()
 	}
 
 	h.SetDV(75.4)
 	h.SetDepth(100)
 	result = sc.Evaluate(h)
-	if result != Collapsed {
+	if result != lifeloss.Collapsed {
 		t.Fail()
 	}
 }
 func Test_RescDamConcreteMasonarySteelStability(t *testing.T) {
 
-	sc := RescDamMasonryConcreteBrick
+	sc := lifeloss.RescDamMasonryConcreteBrick
 
 	h := hazards.DepthandDVEvent{}
 	h.SetDV(35)
 	h.SetDepth(100)
 	result := sc.Evaluate(h)
-	if result != Stable {
+	if result != lifeloss.Stable {
 		t.Fail()
 	}
 
 	h.SetDV(75.4)
 	h.SetDepth(10)
 	result = sc.Evaluate(h)
-	if result != Collapsed {
+	if result != lifeloss.Collapsed {
 		t.Fail()
 	}
 
 	h.SetDV(75.4)
 	h.SetDepth(20)
 	result = sc.Evaluate(h)
-	if result != Stable { //velocity is computed as not being high enough because depth was so high.
+	if result != lifeloss.Stable { //velocity is computed as not being high enough because depth was so high.
 		t.Fail()
 	}
 }
